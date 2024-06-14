50622f66b7ed65813637005fc3402b49a1b47c00
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-06-14 12:14:40 +0300

    toast bg changed

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border bg-primary-2 text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
