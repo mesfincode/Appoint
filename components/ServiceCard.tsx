ba3dd15423a7ecd5ee9ee7e9f8d68f4636018d52
Mesfin Girma <mesfingirmatwo@gmail.com>
2024-06-14 23:27:43 +0300

    added limits to the number of charactors displayed on user name and company name

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -12,11 +12,11 @@ const ServiceCard = ({ profileUrl, name,email, company, serviceDescription ,onCl
          <div className='h-[250px] max-sm:w-full  w-[250px] border-2 border-gray-200 bg-gray-100 transition-all duration-500 hover:bg-primary-2  p-4 rounded-lg cursor-pointer' onClick={onClick} >
             <div className=' flex flex-col gap-1 justify-center  items-center'>
                 <Image src={profileUrl} width={40} height={40} alt={name} style={{ borderRadius: "100%" }} />
-                <h1 className='text-lg font-semibold text-black-2'>{name}</h1>
-                <h1 className='text-xl font-semibold text-black-1'>{company}</h1>
+                <h1 className='  font-semibold text-black-2'>{name.length > 20 ? `${name.slice(0, 20)}...` : name}</h1>
+                <h1 className=' font-semibold text-black-1'>{company}</h1>
                 <h1 className='text-black-2'>{email}</h1>
                 {/* <h1 className='text-center pt-2'>{serviceDescription.slice(0, 100)}</h1> */}
-                <h1 className='text-center pt-1 px-2'>{serviceDescription.length > 50 ? `${serviceDescription.slice(0, 50)}...` : serviceDescription}</h1>
+                <h1 className='text-center pt-1 px-2'>{serviceDescription.length > 30 ? `${serviceDescription.slice(0, 30)}...` : serviceDescription}</h1>
             </div>
 
         </div>
